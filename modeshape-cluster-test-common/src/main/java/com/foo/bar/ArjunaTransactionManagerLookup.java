5b2478d6d43043c9b27aab6e030bedba96d71b1e
Illia Khokholkov <illia.khokholkov@gmail.com>
2017-02-17 12:10:03 -0600

    Set transaction timeout in a correct way.

--- modeshape-cluster-test-common/src/main/java/com/foo/bar/ArjunaTransactionManagerLookup.java
+++ modeshape-cluster-test-common/src/main/java/com/foo/bar/ArjunaTransactionManagerLookup.java
@@ -1,6 +1,7 @@
 package com.foo.bar;
 
-import javax.transaction.SystemException;
+import java.nio.file.Paths;
+
 import javax.transaction.TransactionManager;
 
 import org.modeshape.jcr.api.txn.TransactionManagerLookup;
@@ -15,18 +16,26 @@ import org.modeshape.jcr.api.txn.TransactionManagerLookup;
 public class ArjunaTransactionManagerLookup implements TransactionManagerLookup {
 
     public static final String TIMEOUT_PROPERTY = "transaction.timeout";
+    public static final String RECOVERY_DIR = "transaction.recovery.dir";
+    
+    static {
+        System.setProperty(
+                "com.arjuna.ats.arjuna.common.CoordinatorEnvironmentBean.defaultTimeout",
+                System.getProperty(TIMEOUT_PROPERTY, "3"));
+        
+        System.setProperty(
+                "com.arjuna.ats.arjuna.objectstore.objectStoreDir",
+                System.getProperty(
+                        RECOVERY_DIR,
+                        Paths.get(System.getProperty("user.home"), "arjuna-jta-recovery").toString()));
+    }
     
     @Override
     public TransactionManager getTransactionManager() {
         TransactionManager instance = com.arjuna.ats.jta.TransactionManager.transactionManager();
+        
         if (instance == null) {
             throw new IllegalStateException("Failed to instantiate JBoss JTA transaction manager");
-        }
-        
-        try {
-            instance.setTransactionTimeout(Integer.parseInt(System.getProperty(TIMEOUT_PROPERTY, "3")));
-        } catch (SystemException e) {
-            throw new IllegalStateException("Failed to set global transaction timeout", e);
         }
         
         return instance;
